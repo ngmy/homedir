6c7199074cc7d7671394a8169c61c525bc3bec2a
Yuta Nagamiya <y.nagamiya@gmail.com>
2020-07-13 23:03:19 +0900

    Update the install script and add installation instructions to README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # homedir
+
+## Installation
+
+```bash
+curl -LSs https://raw.githubusercontent.com/ngmy/homedir/master/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+HOMEDIR_PATH="${HOME}/homedir"
+
 doIt() {
-    rsync --exclude=".git" \
-        --exclude=".gitkeep" \
-        --exclude="LICENSE" \
-        --exclude="README.md" \
-        --exclude="install.sh" \
-        -ahv . ~
+    if [ -d "${HOMEDIR_PATH}" ]; then
+        echo "homedir already exists in '${HOMEDIR_PATH}'. Skip download."
+    else
+        echo "Downloading homedir to '${HOMEDIR_PATH}'..."
+        git clone https://github.com/ngmy/homedir.git "${HOMEDIR_PATH}"
+    fi
+    rsync --exclude='.git' \
+        --exclude='.gitkeep' \
+        --exclude='LICENSE' \
+        --exclude='README.md' \
+        --exclude='install.sh' \
+        -ahv "${HOMEDIR_PATH}/" "${HOME}"
 }
 
 if [ "$(uname)" == 'Darwin' ]; then
     # Mac
     doIt
-    ln -fns ~/Documents ~/docs
-    ln -fns ~/Desktop ~/var/desktop
+    ln -fns "${HOME}/Documents" "${HOME}/docs"
+    ln -fns "${HOME}/Desktop" "${HOME}/var/desktop"
 else
     echo "Your platform ($(uname -a)) is not supported."
     exit 1
