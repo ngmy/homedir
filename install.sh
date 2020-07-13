3c747442abb8dd010294012ae25a5cf68138d3ee
Yuta Nagamiya <y.nagamiya@gmail.com>
2020-07-13 23:21:17 +0900

    Convert indentation from 4 spaces into 2 spaces

--- install.sh
+++ install.sh
@@ -3,26 +3,26 @@
 HOMEDIR_PATH="${HOME}/homedir"
 
 doIt() {
-    if [ -d "${HOMEDIR_PATH}" ]; then
-        echo "homedir already exists in '${HOMEDIR_PATH}'. Skip download."
-    else
-        echo "Downloading homedir to '${HOMEDIR_PATH}'..."
-        git clone https://github.com/ngmy/homedir.git "${HOMEDIR_PATH}"
-    fi
-    rsync --exclude='.git' \
-        --exclude='.gitkeep' \
-        --exclude='LICENSE' \
-        --exclude='README.md' \
-        --exclude='install.sh' \
-        -ahv "${HOMEDIR_PATH}/" "${HOME}"
+  if [ -d "${HOMEDIR_PATH}" ]; then
+    echo "homedir already exists in '${HOMEDIR_PATH}'. Skip download."
+  else
+    echo "Downloading homedir to '${HOMEDIR_PATH}'..."
+    git clone https://github.com/ngmy/homedir.git "${HOMEDIR_PATH}"
+  fi
+  rsync --exclude='.git' \
+    --exclude='.gitkeep' \
+    --exclude='LICENSE' \
+    --exclude='README.md' \
+    --exclude='install.sh' \
+    -ahv "${HOMEDIR_PATH}/" "${HOME}"
 }
 
 if [ "$(uname)" == 'Darwin' ]; then
-    # Mac
-    doIt
-    ln -fns "${HOME}/Documents" "${HOME}/docs"
-    ln -fns "${HOME}/Desktop" "${HOME}/var/desktop"
+  # Mac
+  doIt
+  ln -fns "${HOME}/Documents" "${HOME}/docs"
+  ln -fns "${HOME}/Desktop" "${HOME}/var/desktop"
 else
-    echo "Your platform ($(uname -a)) is not supported."
-    exit 1
+  echo "Your platform ($(uname -a)) is not supported."
+  exit 1
 fi
